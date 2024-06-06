f7f6b9debc6f9aec93015fab875b1603528cfff4
tigawanna <denniskinuthiawaweru@gmail.com>
2024-06-06 18:15:20 +0300

    update depenndancies

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-
+import { Suspense } from "react";
 import { PostForm } from "./posts/PostForm";
-import { PostsList } from "./posts/PostsList";
+import { PostsList, PostsListSuspenseFallback } from "./posts/PostsList";
 
 function App() {
   return (
     <div>
       <PostForm />
-      <PostsList />
+      <Suspense fallback={<PostsListSuspenseFallback />}>
+        <PostsList />
+      </Suspense>
     </div>
   );
 }
--- src/posts/PostsList.tsx
+++ src/posts/PostsList.tsx
@@ -6,7 +6,7 @@ export function PostsList({}: PostsListProps) {
     queryKey: ["posts"],
     queryFn: () => {
       return fetch("https://jsonplaceholder.typicode.com/posts").then(
-        (res) => res.json() as Promise<Post[]>,
+        (res) => res.json() as Promise<Post[]>
       );
     },
   });
@@ -17,8 +17,7 @@ export function PostsList({}: PostsListProps) {
         return (
           <div
             key={post.id}
-            className="flex flex-col gap-0.5  p-5 w-full md:w-[44%] lg:w-[30%] hover:bg-base-300 rounded-sm"
-          >
+            className="flex flex-col gap-0.5  p-5 w-full md:w-[44%] lg:w-[30%] hover:bg-base-300 rounded-lg">
             <h2 className="text-2xl font-bold line-clamp-2">{post.title}</h2>
             <p className="text-sm">{post.body}</p>
             <span>by #{post.userId}</span>
@@ -35,4 +34,24 @@ export interface Post {
   id: number;
   title: string;
   body: string;
+}
+
+interface PostsListSuspenseFallbackProps {
+  items?: number;
+}
+
+export function PostsListSuspenseFallback({ items = 12 }: PostsListSuspenseFallbackProps) {
+  return (
+    <div className="w-full h-full flex flex-wrap justify-center gap-2 p-2">
+      {Array(items)
+        .fill(0)
+        .map((_, i) => (
+          <div
+            // biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
+            key={i}
+            className="flex bg-base-200  skeleton  flex-col  h-[200px] gap-0.5  p-5 w-full md:w-[44%] lg:w-[30%]  rounded-lg"
+          />
+        ))}
+    </div>
+  );
 }
